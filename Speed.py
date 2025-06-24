4e51ce85f955202688cdbc72d2a4b13a88e4043d
arrowko <akarrow75@gmail.com>
2025-06-24 10:07:49 +0200

    Update Speed.py

--- Speed.py
+++ Speed.py
@@ -198,7 +198,7 @@ async def main_loop():
         print("🔁 Restarting in 60s...\n")
 
         await send_summary_notification(free_names, duration)
-        await asyncio.sleep(105)
+        await asyncio.sleep(130)
 
 # === Entry Point ===
 
